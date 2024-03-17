a951111fbbea6f8e18190e8d90d65efadd5fa3c6
Max Zermeño Manrique <105751347+mazerman03@users.noreply.github.com>
2024-03-17 00:26:02 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'temporary-2132851',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
